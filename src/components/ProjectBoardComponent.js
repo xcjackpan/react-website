3aaa6ba26ddb0391e783703c5655b63563bb8a2a
xcjackpan <xcjackpan99@gmail.com>
2019-09-02 01:32:32 -0400

    updated projects

--- src/components/ProjectBoardComponent.js
+++ src/components/ProjectBoardComponent.js
@@ -31,52 +31,52 @@ export default class ProjectBoardComponent extends React.Component {
 	 										  tools = "React, Node, Javascript, Firebase"
 	 										  link = "https://github.com/xcjackpan/hackthe6ix"/>
 	 						<ProjectComponent className ="square"
-	 										  projectname = "Always Sunny Soundboard"
-	 										  image = "alwayssunny.png"
-	 										  description = "Simple soundboard. 5.0k+ Play Store installs, 4.1 stars."
-	 										  tools = "React Native"
-	 										  link = "https://github.com/xcjackpan/always-sunny-soundboard"/>
+	 										  projectname = "JAKE"
+	 										  image = "jake.png"
+	 										  description = "Modified NLP keyword extraction."
+	 										  tools = "C++"
+	 										  link = "https://github.com/xcjackpan/jake"/>
 	 					</div>
 	 					<div className="projects-row">
+						 <ProjectComponent className ="square"
+	 										  projectname = "Always Sunny Soundboard"
+	 										  image = "alwayssunny.png"
+	 										  description = "Simple soundboard. 6.0k+ Play Store installs, 4.1 stars."
+	 										  tools = "React Native"
+	 										  link = "https://github.com/xcjackpan/always-sunny-soundboard"/>
 	 						<ProjectComponent className ="square"
 	 										  projectname = "Chroma"
 	 										  image = "chroma.png"
 	 										  description = "Colorful mobile game."
 	 										  tools = "Android Studio, Java"
 	 										  link = "https://github.com/xcjackpan/Chroma"/>
-	 						<ProjectComponent  className ="square"
-	 										  projectname = "reSTARant"
-	 										  image = "restarant.png"
-	 										  description = "Take a photo of a restaurant's sign to see its reviews."
-	 										  tools = "React Native"
-	 										  link = "https://github.com/xcjackpan/restARant"/>
-	 						<ProjectComponent className ="square"
-	 										  projectname = "Puppr"
-	 										  image = "puppr.png"
-	 										  description = "Image gallery that filters out non-dog photos."
-	 										  tools = "React, Javascript"
-	 										  link = "https://github.com/xcjackpan/puppr"/>
-
-	 					</div>
-	 					<div className="projects-row" style={{ paddingBottom: 50 }}>
-	 						<ProjectComponent className ="square"
-	 										  projectname = "Audio Visualiser"
-	 										  image = "visualiser.png"
-	 										  description = "Shows you how a song looks."
-	 										  tools = "Javascript"
-	 										  link = "https://github.com/xcjackpan/AudioAnalyzer"/>
 	 						<ProjectComponent className ="square"
 	 										  projectname = "Dunderbot"
 	 										  image = "dunderbot.png"
 	 										  description = "Text generation using Markov chains"
 	 										  tools = "Javascript"
 	 										  link = "https://github.com/xcjackpan/dunderbot"/>
+
+	 					</div>
+	 					<div className="projects-row" style={{ paddingBottom: 50 }}>
+						 <ProjectComponent  className ="square"
+	 										  projectname = "reSTARant"
+	 										  image = "restarant.png"
+	 										  description = "Take a photo of a restaurant's sign to see its reviews."
+	 										  tools = "React Native"
+	 										  link = "https://github.com/xcjackpan/restARant"/>
+						 <ProjectComponent className ="square"
+	 										  projectname = "Puppr"
+	 										  image = "puppr.png"
+	 										  description = "Image gallery that filters out non-dog photos."
+	 										  tools = "React, Javascript"
+	 										  link = "https://github.com/xcjackpan/puppr"/>
 	 						<ProjectComponent className ="square"
-	 										  projectname = ""
-	 										  image = "tbd.png"
-	 										  description = "I gotta start working on projects in sets of three."
-	 										  tools = ""
-	 										  link = "#"/>
+	 										  projectname = "Audio Visualiser"
+	 										  image = "visualiser.png"
+	 										  description = "Shows you how a song looks."
+	 										  tools = "Javascript"
+	 										  link = "https://github.com/xcjackpan/AudioAnalyzer"/>
 	 					</div>
 					</div>
 			</div>
